76ca58b5deff5053bf0090289a77f41825aa88a2
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-07-19 16:04:41 +0900

    initial commit

---
+++ function.js
@@ -0,0 +1,31 @@
+var timer;
+
+function deleteCode(){
+  let element = document.getElementById("preview");
+  element.innerText = "";
+}
+
+function getCode(){
+    let setName = document.getElementById("taskSet");
+    let num1 = setName.selectedIndex;
+    let str1 = setName.options[num1].value;
+
+    let taskNum = document.getElementById("task");
+    let num2 = taskNum.selectedIndex;
+    let str2 = taskNum.options[num2].value;
+
+    let str = "/" + str1 + "/Task" + num1 + "_" + str2 + ".java";
+    timeer = setInterval('showClock1()',1000);
+    console.log(str);
+}
+
+function showClock1() {
+   let min = parseInt(document.getElementById("minute").innerText, 10);
+   let sec = parseInt(document.getElementById("second").innerText, 10) + 1;
+   document.getElementById("minute").innerHTML = min;
+   document.getElementById("second").innerHTML = sec;
+}
+
+function stopTimer(){
+  clearInterval(timer);
+}
