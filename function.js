db87299ca9348b509ce060d073a13f20cf5b90c2
h-azuma <h-azuma@ist.osaka-u.ac.jp>
2019-08-22 12:09:45 +0900

    update

--- function.js
+++ function.js
@@ -1,48 +1,56 @@
 var timer;
-var showFlg = 0;
+let showFlg = true;
 let min = 0;
 let sec = 0;
 let taskSet = "practice"
 let task = 1;
 let hl = "full";
 let fnt = "nonPropotional";
+let isOnTimer = false;
+let isFinishedTask = true;
 //let setParam = "?taskSet=practice&task=practice&hl=full&font=nonPropotional&view=true";
 //location.search = setParam;
 
 function deleteCode() {
     let code = document.getElementById("preview");
     code.innerText = "";
-    showFlg = 0;
+    showFlg = false;
 }
 
 function getCode(){
-    let taskSetNum = 0;
-    if(taskSet == "control_list"){
-        taskSetNum = 1;
-    }else if(taskSet == "control_string"){
-        taskSetNum = 2;
-    }else if(taskSet == "mathmatical"){
-        taskSetNum = 3;
-    }else if(taskSet == "conditional_branch"){
-        taskSetNum = 4;
-    }
+    if (!isOnTimer) {
+        let taskSetNum = 0;
+        if(taskSet == "control_list"){
+            taskSetNum = 1;
+        }else if(taskSet == "control_string"){
+            taskSetNum = 2;
+        }else if(taskSet == "mathmatical"){
+            taskSetNum = 3;
+        }else if(taskSet == "conditional_branch"){
+            taskSetNum = 4;
+        }
 
-    let filePath = "task/" + taskSet + "/Task" + taskSetNum + "_" + task + ".java";
+        let filePath = "task/" + taskSet + "/Task" + taskSetNum + "_" + task + ".java";
+        let taskNum = parseInt(document.getElementById("presentTask").innerHTML, 10);
     
-    if(showFlg == 0){
-        timer = setInterval('startClock()',1000);
-        showFlg++;
-    }
-    
-    // console.log(filePath);
-    fetch(filePath)
-    .then((response) => response.text())
-    .then((text) => changeHighlight(text))
-    .then(() => changeFont())
-    .catch((error) => console.log(error));
-    
-    let taskNum = parseInt(document.getElementById("presentTask").innerHTML, 10);
-    document.getElementById("presentTask").innerHTML = taskNum + 1;
+        if(!showFlg){
+            console.log("Timer Start")
+            timer = setInterval('startClock()',1000);
+            showFlg = true;
+            isOnTimer = true;
+            document.getElementById("presentTask").innerHTML = taskNum + 1;
+            // console.log(filePath);
+            fetch(filePath)
+            .then((response) => response.text())
+            .then((text) => changeHighlight(text))
+            .then(() => changeFont())
+            .catch((error) => console.log(error));
+        } else {
+            window.alert("次に進むボタンを押してください．");
+        }
+    } else if (isOnTimer) {
+        window.alert("コードはすでに表示されています．");
+    }　
 }
 
 function startClock() {
@@ -62,7 +70,7 @@ function startClock() {
     if(sec < 10){
         logsec = '0' + sec;
     }
-    console.log(logmin + ':' + logsec);
+    // console.log(logmin + ':' + logsec);
 }
 
 function resetClock(){
@@ -71,16 +79,28 @@ function resetClock(){
 }
 
 function stopTimer(){
-    window.clearInterval(timer);
-    if(min < 10){
-        min = '0' + min;
-   }
+    if (isOnTimer) {
+        window.clearInterval(timer);
+        if(min < 10){
+            min = '0' + min;
+        }
 
-   if(sec < 10){
-       sec = '0' + sec;
-   }
-    window.alert(min + ':' + sec);
-    resetClock();
+        if(sec < 10){
+            sec = '0' + sec;
+        }
+        window.alert(min + ':' + sec);
+        isOnTimer = false;
+        isFinishedTask = true;
+        resetClock();
+    } else {
+        if (isFinishedTask) {
+            window.alert("次に進むボタンを押してください．");
+        } else {
+            window.alert("表示ボタンを押してください．");
+        }
+        
+    }
+    
 }
 
 function goNextTask(){
@@ -107,79 +127,95 @@ function goNextTask(){
     console.log(param);
     location.search = param;
     */
-    deleteCode();
+    if (showFlg && isFinishedTask) {
+        deleteCode();
+        isFinishedTask = false;
     
-    if(taskSet == "practice"){
-        taskSet = "control_list";
-        document.getElementById("taskSet").innerHTML = "セット1";
-        document.getElementById("task").innerHTML = "タスク" + task;
-    }else{
-        if(task == 6){
-            switch(taskSet){
-                case "control_list":
-                    taskSet = "control_string";
-                    document.getElementById("taskSet").innerHTML = "セット2";
+        if(taskSet == "practice"){
+            taskSet = "control_list";
+            document.getElementById("taskSet").innerHTML = "セット1";
+            document.getElementById("task").innerHTML = "タスク" + task;
+        }else{
+            if(task == 6){
+                switch(taskSet){
+                    case "control_list":
+                        taskSet = "control_string";
+                        document.getElementById("taskSet").innerHTML = "セット2";
+                        break;
+                    case "control_string":
+                        taskSet = "mathmatical";
+                        document.getElementById("taskSet").innerHTML = "セット3";
+                        break;
+                    case "mathmatical":
+                        taskSet = "conditional_branch";
+                        document.getElementById("taskSet").innerHTML = "セット4";
+                        break;
+                }
+                task = 0;
+            }
+            task++;
+            switch(task){
+                case 1:
+                    hl = "full";
+                    fnt = "nonPropotional";
                     break;
-                case "control_string":
-                    taskSet = "mathmatical";
-                    document.getElementById("taskSet").innerHTML = "セット3";
+                case 2:
+                    hl = "preserved";
+                    fnt = "nonPropotional";
                     break;
-                case "mathmatical":
-                    taskSet = "conditional_branch";
-                    document.getElementById("taskSet").innerHTML = "セット4";
+                case 3:
+                    hl = "gray";
+                    fnt = "nonPropotional";
+                    break;
+                case 4:
+                    hl = "random";
+                    fnt = "nonPropotional";
+                    break;
+                case 5:
+                    hl = "preserved";
+                    fnt = "propotional";
+                    break;
+                case 6:
+                    hl = "preserved";
+                    fnt = "kawaii";
                     break;
             }
-            task = 0;
-        }
-        task++;
-        switch(task){
-            case 1:
-                hl = "full";
-                fnt = "nonPropotional";
-                break;
-            case 2:
-                hl = "preserved";
-                fnt = "nonPropotional";
-                break;
-            case 3:
-                hl = "gray";
-                fnt = "nonPropotional";
-                break;
-            case 4:
-                hl = "random";
-                fnt = "nonPropotional";
-                break;
-            case 5:
-                hl = "preserved";
-                fnt = "propotional";
-                break;
-            case 6:
-                hl = "preserved";
-                fnt = "kawaii";
-                break;
-        }
         
-        document.getElementById("task").innerHTML = "タスク" + task;
-        switch(highlight){
-            case "full":
-                document.getElementById("highlight").innerHTML = "フルハイライト";
-            case "preserved":
-                document.getElementById("highlight").innerHTML = "予約語";
-            case "gray":
-                document.getElementById("highlight").innerHTML = "白黒";
-            case "random":
-                document.getElementById("highlight").innerHTML = "ランダム";
-        }
+            document.getElementById("task").innerHTML = "タスク" + task;
+            switch(hl){
+                case "full":
+                    document.getElementById("highlight").innerHTML = "フルハイライト";
+                    break;
+                case "preserved":
+                    document.getElementById("highlight").innerHTML = "予約語";
+                    break;
+                case "gray":
+                    document.getElementById("highlight").innerHTML = "白黒";
+                    break;
+                case "random":
+                    document.getElementById("highlight").innerHTML = "ランダム";
+                    break;
+            }
         
-        switch(font){
-            case "nonPropotional":
-                document.getElementById("font").innerHTML = "等幅フォント";
-            case "propotional":
-                document.getElementById("font").innerHTML = "非等幅フォント";
-            case "kawaii":
-                document.getElementById("font").innerHTML = "Kawaiiフォント";    
+            switch(fnt){
+                case "nonPropotional":
+                    document.getElementById("font").innerHTML = "等幅フォント";
+                    break;
+                case "propotional":
+                    document.getElementById("font").innerHTML = "非等幅フォント";
+                    break;
+                case "kawaii":
+                    document.getElementById("font").innerHTML = "Kawaiiフォント";
+                    break;
+            }
         }
+    } else if (!showFlg){
+        window.alert("表示ボタンを押してください．");
+    } else if (!isFinishedTask) {
+        window.alert("現在のタスクを終了してください．");
     }
+    
+    
 }
 
 function changeFont(){
